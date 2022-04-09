6b08a479da970eb91245c4048ee4fddf0f177cd7
Talib <95438964+talibhussain490@users.noreply.github.com>
2022-04-10 04:36:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,13 +13,10 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer>Learn React
         </a>
       </header>
     </div>
   );
 }
-
 export default App;

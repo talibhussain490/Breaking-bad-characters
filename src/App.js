cd231a8401990b905a6f12872958a4c942827d42
talib <talibhussain490@gmail.com>
2022-04-11 23:12:28 +0530

    update 02

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   const { loading, currentCharacters, showIndividual } =
     useContext(DataContext);
   return (
-<<<<<<< HEAD
     <>
       <Header />
       <Search />
@@ -27,22 +26,6 @@ function App() {
         <Pagination />
       ) : null}
     </>
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer>Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 6b08a479da970eb91245c4048ee4fddf0f177cd7
   );
 }
 export default App;

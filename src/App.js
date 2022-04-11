199c985bc82c0194315df2ba400658521c7a13c8
talib <talibhussain490@gmail.com>
2022-04-11 22:52:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import CharacterList from "./CharacterList";
+import IndividualCharacter from "./IndividualCharacter";
+import Pagination from "./Pagination";
+import Search from "./Search";
+import Loader from "./Loader";
+import DataContext from "./DataContext";
+import { useContext } from "react";
+import "./App.css";
 
 function App() {
+  const { loading, currentCharacters, showIndividual } =
+    useContext(DataContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Search />
+      {showIndividual ? (
+        <IndividualCharacter />
+      ) : loading ? (
+        <Loader />
+      ) : (
+        <CharacterList />
+      )}
+      {currentCharacters.length < 10 ? null : !showIndividual ? (
+        <Pagination />
+      ) : null}
+    </>
   );
 }
 
